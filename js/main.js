99c3f041de60cc684a04ebf2d7e86c7a4c611568
MatteoGaloni <mtgaloni@gmail.com>
2023-05-02 14:57:08 +0200

    add object array

---
+++ js/main.js
@@ -0,0 +1,31 @@
+// creo array con oggetti e info forniti
+teamInfo = [
+  {
+    nome: [
+      "Wayne Barnett",
+      "Angela Caroll",
+      "Walter Gordon",
+      "Angela Lopez",
+      "Scott Estrada",
+      "Barbara Ramos",
+    ],
+    ruolo: [
+      "Founder & CEO",
+      "Chief Editor",
+      "Office Manager",
+      "Social Media Manager",
+      "Developer",
+      "Graphic Designer",
+    ],
+    foto: [
+      "wayne-barnett-founder-ceo.jpg",
+      "angela-caroll-chief-editor.jpg",
+      "walter-gordon-office-manager.jpg",
+      "angela-lopez-social-media-manager.jpg",
+      "scott-estrada-developer.jpg",
+      "barbara-ramos-graphic-designer.jpg",
+    ],
+  },
+];
+
+console.log(teamInfo);
