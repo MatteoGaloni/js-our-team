94fee3ae8463c776aee2d52f43360e81dc4d1beb
MatteoGaloni <mtgaloni@gmail.com>
2023-05-06 21:52:19 +0200

    add Html content using ForEach function

--- js/main.js
+++ js/main.js
@@ -1,31 +1,49 @@
 // creo array con oggetti e info forniti
-teamInfo = [
+team = [
   {
-    nome: [
-      "Wayne Barnett",
-      "Angela Caroll",
-      "Walter Gordon",
-      "Angela Lopez",
-      "Scott Estrada",
-      "Barbara Ramos",
-    ],
-    ruolo: [
-      "Founder & CEO",
-      "Chief Editor",
-      "Office Manager",
-      "Social Media Manager",
-      "Developer",
-      "Graphic Designer",
-    ],
-    foto: [
-      "wayne-barnett-founder-ceo.jpg",
-      "angela-caroll-chief-editor.jpg",
-      "walter-gordon-office-manager.jpg",
-      "angela-lopez-social-media-manager.jpg",
-      "scott-estrada-developer.jpg",
-      "barbara-ramos-graphic-designer.jpg",
-    ],
+    nome: "Wayne Barnett",
+    ruolo: "Founder & CEO",
+    foto: "wayne-barnett-founder-ceo.jpg",
+  },
+  {
+    nome: "Angela Caroll",
+    ruolo: "Chief Editor",
+    foto: "angela-caroll-chief-editor.jpg",
+  },
+  {
+    nome: "Walter Gordon",
+    ruolo: "Office Manager",
+    foto: "walter-gordon-office-manager.jpg",
+  },
+  {
+    nome: "Angela Lopez",
+    ruolo: "Social Media Manager",
+    foto: "angela-lopez-social-media-manager.jpg",
+  },
+  {
+    nome: "Scott Estrada",
+    ruolo: "Developer",
+    foto: "scott-estrada-developer.jpg",
+  },
+  {
+    nome: "Barbara Ramos",
+    ruolo: "Graphic Designer",
+    foto: "barbara-ramos-graphic-designer.jpg",
   },
 ];
 
-console.log(teamInfo);
+// ***********For each Test********************
+team.forEach((element) => {
+  teamMember = element;
+  const contentElement = document.getElementById("row-container");
+  let contentHtml = `<div class="col-4">`;
+  contentHtml += `<div class="card" style="width: 18rem;">`;
+  contentHtml += `<img src="./img/${teamMember.foto}" alt="${teamMember.nome}" />`;
+  contentHtml += `<div class="card-body">`;
+  contentHtml += `<h5 id="nome" class="card-title">${teamMember.nome}</h5>`;
+  contentHtml += `<p id="ruolo" class="card-text">${teamMember.ruolo}</p>`;
+  contentHtml += `</div>`;
+  contentHtml += `</div>`;
+  contentHtml += `</div>`;
+  contentElement.innerHTML += contentHtml;
+});
